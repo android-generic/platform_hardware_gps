cd5e681ce60c2ecbac5262bb4a7ab6943019cc08
Michael Goffioul <michael.goffioul@gmail.com>
2023-03-30 15:05:43 +0200

    Make HAL compatible with Treble
    - install HAL as vendor module
    - add dependency on libhardware_headers
    - add -Wno-literal-range for gps.huawei

--- Android.mk
+++ Android.mk
@@ -7,19 +7,23 @@ LOCAL_PATH := $(call my-dir)
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_MODULE := gps.default
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := gps.c
 LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_MODULE := gps.huawei
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := gps_huawei.c
-LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable -Wno-literal-range
 include $(BUILD_SHARED_LIBRARY)
 endif
